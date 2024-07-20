c7f564454e2be6cd9a2f50f62c6bb5dfa5ae974e
Neel0264 <neeljayeshbhai.moradiya@edu.sait.ca>
2024-07-19 21:09:15 -0600

    Delete styles directory

--- styles/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('#showTextButton');
-    const additionalText = document.querySelector('#additionalText');
-    
-    button.addEventListener('click', function() {
-        additionalText.style.display = 'block';
-    });
-});
-
